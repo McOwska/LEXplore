684de94f014ca28815a5f7f71858263ac9ee713a
mcowska <zuza.m.makowska@gmail.com>
2025-08-07 13:31:28 +0200

    header

--- lexplore-ui/src/App.js
+++ lexplore-ui/src/App.js
@@ -1,6 +1,6 @@
 import TextFileLoader from './components/TextFileLoader';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
   return (
--- lexplore-ui/src/components/Header/Header.js
+++ lexplore-ui/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { useIntl } from 'react-intl';
+import styles from './Header.module.css';
 
 const Header = () => {
     const intl = useIntl();
